e3eeefe3b0b4dfc6b33456e7da4a541064851594
Binuyo Mayowa peter <binuyomayor16@gmail.com>
2026-06-24 11:56:47 +0100

    Initial Commit

---
+++ src/main/java/org/globalpay/JavaFX/Project/Main.java
@@ -0,0 +1,17 @@
+package org.globalpay.JavaFX.Project;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+
+    public static void main() {
+        int nums[] = {5,7,9,11,13};
+        int target = 11;
+
+        result = linearSearch(nums, target);
+    }
+
+    private static int linearSearch(int[] nums, int target) {
+
+    }
+}
