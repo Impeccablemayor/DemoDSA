3d08870e0fcca3393ad3552f2e031f18922c0b37
Binuyo Mayowa peter <binuyomayor16@gmail.com>
2026-06-24 12:25:16 +0100

    I did linearSearch and BinarySearch

--- src/main/java/org/globalpay/JavaFX/Project/Main.java
+++ src/main/java/org/globalpay/JavaFX/Project/Main.java
@@ -5,15 +5,48 @@ package org.globalpay.JavaFX.Project;
 public class Main {
 
     public static void main() {
-        int nums[] = {5,7,9,11,13};
+        int[] nums = {5,7,9,11,13};
         int target = 11;
 
         int result = linearSearch(nums, target);
+        int binarySearch = binarySearch(nums, target);
 
-        System.out.println(result);
+        System.out.println("Element found at index " + binarySearch);
+
+
+        if (result != -1) {
+            System.out.println("Element found at index " + result);
+        }else {
+            System.out.println("Element not found");
+        }
     }
 
-    private static int linearSearch(int[] nums, int target) {
+    public static int linearSearch(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == target) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static int binarySearch(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length-1;
+
+        while (left < right) {
+            int mid = (right + left) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            }
+            else if (nums[mid] < target) {
+                left = mid + 1;
+            }
+            else  {
+                right = mid -1;
+            }
+        }
         return -1;
     }
 }
