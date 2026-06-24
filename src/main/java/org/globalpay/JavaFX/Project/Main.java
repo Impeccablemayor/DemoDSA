8f2d77010bad0c7ef0d7de1ab4cec9a7a12044ed
Binuyo Mayowa peter <binuyomayor16@gmail.com>
2026-06-24 14:50:38 +0100

    Did some sorting with the bubble sort Algorithm

--- src/main/java/org/globalpay/JavaFX/Project/Main.java
+++ src/main/java/org/globalpay/JavaFX/Project/Main.java
@@ -7,9 +7,14 @@ public class Main {
     public static void main() {
         int[] nums = {5,7,9,11,13};
         int target = 11;
+        int left = 0;
+        int right = nums.length-1;
 
         int result = linearSearch(nums, target);
         int binarySearch = binarySearch(nums, target);
+        int recursiveBinarySearch = recursiveBinarySearch(nums, target, left, right);
+
+        System.out.println("Element was found at 'Recursive'" + recursiveBinarySearch);
 
         System.out.println("Element found at index " + binarySearch);
 
@@ -47,6 +52,21 @@ public class Main {
                 right = mid -1;
             }
         }
+        return -1;
+    }
+
+    public static int recursiveBinarySearch(int[] nums, int target,int left,int right ) {
+
+        int mid = (right + left) / 2;
+        if (left <= right)
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                recursiveBinarySearch(nums, target, nums[mid] + 1, right);
+            } else {
+                recursiveBinarySearch(nums, target, left, nums[mid] - 1);
+            }
+
         return -1;
     }
 }
